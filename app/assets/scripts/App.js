b60e84ceb10ba41424f0b9a03381a5239c80142a
Jinx <chisel.delahanty@gmail.com>
2021-08-29 22:15:32 +0200

    Set up UX for modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,14 @@ import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 if (module.hot) {
     module.hot.accept();
 }
 
+new Modal();
 let stickyHeader = new StickyHeader();
-
 let mobileMenu = new MobileMenu();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
